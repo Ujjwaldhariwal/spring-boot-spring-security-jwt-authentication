ce4ea4ec1953d69d1c48576b2d6374595a3184fa
Ujjwal <ujjwaldhariwal0@gmail.com>
2024-02-26 12:29:17 +0530

    Add files via upload

---
+++ spring-boot-spring-security-jwt-authentication-master/spring-boot-spring-security-jwt-authentication-master/src/main/java/com/Auth/springjwt/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.Auth.springjwt.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Auth.springjwt.models.ERole;
+import com.Auth.springjwt.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
